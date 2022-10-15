034cfacaa654393c7d8994b83dea7491d038739a
Juan Alegría <juanszalegria@gmail.com>
2022-10-15 10:09:23 -0500

    Feat: add email config

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -135,8 +135,8 @@ STATICFILES_DIRS = (
 )
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.office365.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'j.alegria@uniandes.edu.co'
+EMAIL_HOST_USER = 'surfi.team@gmail.com'
 EMAIL_HOST_PASSWORD = 'xxxxxxxxx'
