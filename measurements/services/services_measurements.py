0e8041abc30d4a5489e4d0da44edfd556bbc06e6
Tomas1307 <t.acosta@uniandes.edu.co>
2022-10-16 16:59:46 -0500

    Envio de e-mail a tomas

--- measurements/services/services_measurements.py
+++ measurements/services/services_measurements.py
@@ -4,6 +4,7 @@ from django.core.mail import send_mail
 def check_alarm(value):
     if value >= 42:
         send_email()
+        send_email2()
     return()
 
 def send_email():
@@ -11,3 +12,9 @@ def send_email():
     message = 'Warning!!! the temperature is growing'
     recepient = "j.alegria@uniandes.edu.co"
     send_mail(subject, message, EMAIL_HOST_USER, [recepient], auth_password=EMAIL_HOST_PASSWORD)
+
+def send_email2():
+    subject = 'Test Taller'
+    message = 'Warning!!! the temperature is growing'
+    recepient = "t.acosta@uniandes.edu.co"
+    send_mail(subject, message, EMAIL_HOST_USER, [recepient], auth_password=EMAIL_HOST_PASSWORD)    
